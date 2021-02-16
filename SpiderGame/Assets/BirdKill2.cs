fda1a51d92e24770249aaa0c2e6775328c47a29d
Michael Brennen <56268235+mb030317@users.noreply.github.com>
2021-02-16 11:40:46 -0500

    bird attack

--- SpiderGame/Assets/BirdKill2.cs
+++ SpiderGame/Assets/BirdKill2.cs
@@ -5,13 +5,20 @@ using UnityEngine;
 public class BirdKill2 : MonoBehaviour
 {
     public GameObject deathText;
+    public float speed = 5.0f;
+    public Transform spider;
 
     public void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.name == "Spider")
         {
+            float step = speed * Time.deltaTime;
+            spider = other.gameObject.transform;
+            this.gameObject.GetComponent<Animator>().enabled = false;
+            transform.position = Vector3.MoveTowards(transform.position, spider.position, step);
             Destroy(other.gameObject);
             deathText.SetActive(true);
+            this.gameObject.transform.position = other.transform.position;
         }
     }
 }
